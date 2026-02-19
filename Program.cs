162fae67cc7f97d19eefd2a9af0a334885e058f1
dayvidcristiano <dayvidcristianosilva@gmail.com>
2026-02-19 18:00:28 -0300

    Projeto Final - Atualização do Docker

--- Program.cs
+++ Program.cs
@@ -3,10 +3,12 @@ using sistema_de_monitoramento.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
